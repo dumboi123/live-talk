5aac8a84e3754f65184e6533f47fc4ac920be684
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-10-13 12:47:19 +0700

    feat: Add Avatar component with fallback and image support

--- frontend/src/components/layout/NavigationBar.tsx
+++ frontend/src/components/layout/NavigationBar.tsx
@@ -1,5 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { MessageCircleMore, BookUser, Settings } from "lucide-react";
 export default function NavigationBar() {
   return (
-    <div className="bg-gray-800 text-white h-full w-20">NavigationBar</div>
+    <div className="flex flex-col bg-gray-800 text-white h-full w-20 py-4  justify-between">
+      <div className="flex flex-col gap-8 items-center">
+        <Avatar className="w-10 h-10">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Button variant="ghost" className="[&>svg]:!w-9 [&>svg]:!h-9">
+          <MessageCircleMore color="#fff" strokeWidth={1.5} />
+        </Button>
+        <Button variant={"ghost"} className="[&>svg]:!w-9 [&>svg]:!h-9">
+          <BookUser color="#ffffff" strokeWidth={1.5} />
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-8 items-center">
+        <Button variant={"ghost"} className="[&>svg]:!w-9 [&>svg]:!h-9">
+          <Settings color="#ffffff" strokeWidth={1.5} />
+        </Button>
+      </div>
+    </div>
   );
 }
