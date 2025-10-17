fbe23b4c6f10c3af8c4be4505004ecba9a875e36
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-10-17 23:15:14 +0700

    add emoji picker

--- frontend/src/components/conversationZone/ChatInput.tsx
+++ frontend/src/components/conversationZone/ChatInput.tsx
@@ -1,21 +1,32 @@
-'use client'
+"use client";
 import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
-
+import { Smile, ThumbsUp } from "lucide-react";
 export default function ChatInput() {
   const [message, setMessage] = useState("");
 
   return (
-    <div className="w-full bg-gray-900 flex items-end">
+    <div className="w-full bg-gray-800 flex items-center gap-3 p-2">
       <TextareaAutosize
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder="Enter your message..."
-        minRows={2}
+        minRows={1}
         maxRows={6} // tương ứng với khoảng 220px
         className="flex-1 resize-none overflow-y-auto bg-gray-800 text-white p-3 outline-none"
+      />
+      <Smile
+        size={32}
+        className="text-gray-300 cursor-pointer hover:text-white hover:scale-110 transition-transform"
+        onClick={() => console.log("Emoji picker opened")}
+      />
+
+      <img
+        src="https://twemoji.maxcdn.com/v/latest/72x72/1f604.png"
+        alt="Emoji"
+        className="w-[28px] h-[28px] cursor-pointer hover:scale-110 transition-transform"
+        onClick={() => console.log("Emoji clicked")}
       />
     </div>
   );
 }
-// h-[65px]
